9267081b99cdfe0bc758ac1c3cafd2ece721ae3e
Nana Yaw Sasu Appiah-Miracle <nanasasu@MacBook-Air.local>
2025-12-25 15:47:27 +0000

    Fix useSearchParams Suspense boundary error - wrap TopLoadingBar in Suspense

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Suspense } from 'react'
 import { SessionProvider } from 'next-auth/react'
 import { TopLoadingBar } from '@/components/ui/top-loading-bar'
 import { Toaster } from '@/components/ui/toaster'
@@ -9,7 +10,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ThemeProvider>
-        <TopLoadingBar />
+        <Suspense fallback={null}>
+          <TopLoadingBar />
+        </Suspense>
         <Toaster />
         {children}
       </ThemeProvider>
